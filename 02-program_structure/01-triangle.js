5ec8cd3f6c5dad3020a527fa1aefdef4062d3873
Igor Galić <i.galic@brainsware.org>
2015-04-20 15:48:29 +0200

    explain why i monkeypatch String

--- 02-program_structure/01-triangle.js
+++ 02-program_structure/01-triangle.js
@@ -1,3 +1,6 @@
+// i know that this hasn't been taught in the book, but
+// it felt like the right way to do it.
+
 String.prototype.repeat = function(times) {
    return (new Array(times + 1)).join(this);
 };
