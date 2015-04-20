eb08052249ccea4cbbf8840119b390b0bd079ffb
Igor Galić <i.galic@brainsware.org>
2015-04-20 15:48:43 +0200

    re-assign, rather than modifying in-place
    feels less wrong.

--- 02-program_structure/02-fizzbuzz.js
+++ 02-program_structure/02-fizzbuzz.js
@@ -3,7 +3,7 @@ for (i = 1; i <= 100; i++) {
   if (i % 3 === 0) {
     result = 'Fizz';
     if (i % 5 === 0) {
-      result += 'Buzz';
+      result = 'FizzBuzz';
     }
   } else if (i % 5 === 0) {
     result = 'Buzz';
